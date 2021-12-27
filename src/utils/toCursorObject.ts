30b4ebdb3e6fe382b465d25bfcf0661ac96ad621
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-12-26 21:38:52 -0800

    fix(utils): handle invalid cursor

--- src/utils/toCursorObject.ts
+++ src/utils/toCursorObject.ts
@@ -1,7 +1,14 @@
 import { Cursor } from '../types/cursor';
+import { UserInputError } from 'apollo-server-errors';
 
 const toCursorObject = (cursor: string) => {
-  return JSON.parse(Buffer.from(cursor, 'base64').toString('ascii')) as Cursor;
+  try {
+    return JSON.parse(
+      Buffer.from(cursor, 'base64').toString('ascii')
+    ) as Cursor;
+  } catch {
+    throw new UserInputError('Invalid cursor.');
+  }
 };
 
 export default toCursorObject;
